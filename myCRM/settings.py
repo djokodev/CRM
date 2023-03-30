6259daf522c16928e7fa9d214e943a521f0adf69
djokodev <djokodev75@gmail.com>
2023-03-30 14:19:43 +0200

    affichage de la page home

--- myCRM/settings.py
+++ myCRM/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "customers",
+    "products",
+    "orders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
