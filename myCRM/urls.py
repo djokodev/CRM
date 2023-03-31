a0e1186182e7cc9c4ad945976b97af3811ae50e9
djokodev <djokodev75@gmail.com>
2023-03-31 05:17:55 +0200

    insertion du code des templates

--- myCRM/urls.py
+++ myCRM/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("products.urls")),
+    path("customers/", include("customers.urls")),
+    path("orders/", include("orders.urls")),
     path("admin/", admin.site.urls),
 ]
